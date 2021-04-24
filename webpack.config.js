7d19150c5bd8f8e92a3206cebfbe64c001ad5b78
alex <gaunyu.wu@orange.com>
2021-04-24 16:42:34 +0800

    add development watch true webpack-dev-server webpack-dev-middleware  to run a serve and serve static file

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
+  },
+  devServer: {
+    contentBase: "./dist",
   },
   output: {
     filename: "[name].bundle.js",
@@ -14,7 +19,7 @@ module.exports = {
   module: {},
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "Development",
     }),
   ],
 };
