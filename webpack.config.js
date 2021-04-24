c59da5fb8393ed228302bccceb4eafd019f58ddc
alex <gaunyu.wu@orange.com>
2021-04-24 21:37:29 +0800

    add caching change cotenthash and optimization config obj

--- webpack.config.js
+++ webpack.config.js
@@ -6,38 +6,32 @@ module.exports = {
   devtool: "inline-source-map",
   entry: {
     index: "./src/index.js",
-    another: "./src/another-module.js",
-    // index: {
-    //   import: "./src/index.js",
-    //   dependOn: "shared",
-    // },
-    // another: {
-    //   import: "./src/another-module.js",
-    //   dependOn: "shared",
-    // },
-    // shared: "lodash",
   },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-  },
-  //不加这个发现也可以
-  // optimization: {
-  //   runtimeChunk: "single",
-  // },
   devServer: {
     contentBase: "./dist",
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
   module: {},
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Development",
+      title: "Caching",
     }),
   ],
 };
